64555c5c9e19bc40bb08afccbdf46a6d6dfba7c0
Mohammad Alfayoume <109729591+mohammadalfayoume@users.noreply.github.com>
2025-09-22 09:39:27 +0300

    Update CategoryBrief.cs

--- LibraryManagementSystem.Application/DTOs/Category/CategoryBrief.cs
+++ LibraryManagementSystem.Application/DTOs/Category/CategoryBrief.cs
@@ -1,8 +1,8 @@
-﻿using LibraryManagementSystem.Application.Mappings;
+using LibraryManagementSystem.Application.Mappings;
 
 namespace LibraryManagementSystem.Application.DTOs
 {
-    public class CategoryBrief : IMapFrom<Domain.Entities.Category>
+    public class CategoryBrief : IMapFrom<Domain.Entities.Category>, IMapFrom<Domain.Entities.BookCategory>
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
